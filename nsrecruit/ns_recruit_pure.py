3fa344cc067a30c95486b6526f6fba8bffb7a1c6
Michael Richards <fluffy.trooper.2001@gmail.com>
2021-05-13 14:34:09 +1000

    end of data review implementation.
    StopIteration replaced with embed showing previous results.

--- nsrecruit/ns_recruit_pure.py
+++ nsrecruit/ns_recruit_pure.py
@@ -15,24 +15,36 @@ def nope(nation):
 async def recruit(channel):
     """Gives a list of up to 8 nations to send recruitment telegrams"""
 
-    h = await nsapi("happenings", limit=str(), filter='founding')
+    h = await nsapi("happenings", limit=str(15+len(done)*15), filter='founding')
     events = x2d.parse(pretty_string(h))["WORLD"]["HAPPENINGS"]["EVENT"]
 
     nations, e_iter = list(), events.__iter__()
     new = lambda: e_iter.__next__()['TEXT'].split("@@")[1]
     nation = new()
     for n in range(8):
-        while nope(nation): nation = new()
+        try:
+            while nope(nation): nation = new()
+        except (StopIteration, RuntimeError): break
         (done.append(nation), nations.append(nation))
-    print(nations)
+    assert nations
     return await channel.send(', '.join(nations))
 
 @client.event
 async def on_ready():print('running.')
+async def review(channel, e):
+    e.title, d_iter = "Run out of noobs.", done.__iter__()
+    for i in range(len(done)//8+1):
+      try:
+        e.add_field(name=str(i+1), value=', '.join([d_iter.__next__()for _ in range(8)]), inline=True)
+      except StopIteration:
+        e.add_field(name=str(len(done)//8+1), value=', '.join(done[-(len(done)%8):]), inline=True)
+    await channel.send(embed=e)
 
 @client.event
 async def on_message(message):
   if message.content.startswith('!'):
     if message.content[1:].startswith("recruit"):
-      await recruit(message.channel)
+      try: await recruit(message.channel)
+      except:
+          await review(message.channel, __import__('discord').Embed(color=0xff6600))
     else: print(message.content[1:], 'is not a command')
